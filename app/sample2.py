03cf861b3792a879430a08e57dfad7b771b027e4
suttang <suttang@gmail.com>
2017-06-22 16:20:38 +0900

    real-image

--- app/sample2.py
+++ app/sample2.py
@@ -6,13 +6,16 @@ from SimpleCV import *
 if __name__ == '__main__':
 
     # Load and show the main image
-    main = Image("image.jpg")
+    main = Image('image2.jpg')
+
+    bg = Image('background.jpg')
 
     # Create the mask to apply and show the mask
-    mask = main.hueDistance(color=(118, 251, 77), minsaturation=100, minvalue=80).binarize()
+    # mask = main.hueDistance(color=Color.GREEN, minsaturation=170, minvalue=80).binarize()
+    mask = main.hueDistance(color=(0, 192, 95), minsaturation=130, minvalue=35).binarize()
 
-    mask.save('output-mask2.jpg')
+    mask.save('mask.jpg')
 
     # Combine the mask and other images to get the final result
-    result = (main - mask)
-    result.save('output2.jpg')
+    result = (main - mask) + (bg - mask.invert())
+    result.save('output.jpg')
